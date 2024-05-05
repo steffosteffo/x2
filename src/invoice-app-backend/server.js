4da8cd12dc1447a9784b8593a636573d5ebd665e
steffo <steffo@steffo.live>
2024-05-05 09:39:23 +0200

    3000 portt server.js

--- src/invoice-app-backend/server.js
+++ src/invoice-app-backend/server.js
@@ -7,30 +7,70 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
 
-// MongoDB Connection
-// Connect to MongoDB
-//const MONGO_URI = 'YOUR_MONGO_CONNECTION_URI';
+// OLD START///////////////////////
+
+/*
+app.use(cors());
 
 const MONGO_URI='mongodb+srv://root:root@books-store-mern.ipulw5t.mongodb.net/steffo?retryWrites=true&w=majority'
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => console.log('Connected to MongoDB'));
+// Define Routes
+app.use('/api/products', require('./routes/products'));
+app.use('/api/customers', require('./routes/customers'));
+app.use('/api/invoices', require('./routes/invoices'));
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+*/
+// OLD END///////////////////////
 
 
 
 
+///NYTT ///////////////////////////////////START 
+///NYTT ///////////////////////////////////START 
+///NYTT ///////////////////////////////////START 
 
 
+
+
+// Allow requests from specific origin (e.g., GitHub Pages)
+app.use(cors({
+  origin: 'https://steffosteffo.github.io',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+
+const MONGO_URI='mongodb+srv://root:root@books-store-mern.ipulw5t.mongodb.net/steffo?retryWrites=true&w=majority'
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => console.log('Connected to MongoDB'));
+// Your API endpoints
+app.get('/x2', (req, res) => {
+  // Handle request and respond with data
+  res.json({ message: 'Data from MongoDB' });
+});
 
 // Define Routes
 app.use('/api/products', require('./routes/products'));
 app.use('/api/customers', require('./routes/customers'));
 app.use('/api/invoices', require('./routes/invoices'));
 
-// Start Server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
+///NYTT ///////////////////////////////////SLUT
+///NYTT ///////////////////////////////////SLUT
+///NYTT ///////////////////////////////////SLUT
+///NYTT ///////////////////////////////////SLUT
