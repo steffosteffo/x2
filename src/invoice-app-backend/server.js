2f11569204b0f131c909fdc9b01ec1ff6d1507ba
steffo <steffo@steffo.live>
2024-05-21 12:57:31 +0200

    5000

--- src/invoice-app-backend/server.js
+++ src/invoice-app-backend/server.js
@@ -92,8 +92,8 @@ app.get('/api/customers', async (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(3000,'0.0.0.0', () => {
+  console.log('Server running on port 3000 ${PORT}');
 });
 
 
